df5d6e6ab2565e5dc0b55405d04dc9cb174193a8
Robert Olteanu <91191507+olteanurobert@users.noreply.github.com>
2024-02-22 11:26:21 +0200

    Header desktop responsive

--- app/components/clientComponents/header.tsx
+++ app/components/clientComponents/header.tsx
@@ -22,80 +22,80 @@ const Header = () => {
     useActiveSectionContext();
 
   return (
-    <header className="z-[999] relative">
-      <div>
-        <div className="fixed sm:top-[1.3rem] sm:h-[3.25rem] sm:w-[50rem]">
-          <div className="flex items-center gap-2">
-            <Image src={logo} alt="" className="w-36" />
-          </div>
+    <header className="flex justify-between items-center px-6 pt-2 pb-6">
+      <div className="">
+        <div className="">
+          <Image src={logo} alt="" className="w-20" />
         </div>
-        <motion.div
-          className="flex justify-center items-center fixed top-0 left-1/2 h-[4.5rem] w-full sm:top-[1.3rem] sm:h-[3.25rem] sm:w-[50rem] sm:rounded-full"
-          initial={{ y: -100, x: "-50%", opacity: 0 }}
-          animate={{ y: 0, x: "-50%", opacity: 1 }}
-        >
-          <div className="flex items-center gap-2">
-            <BsFacebook />
-            facebook
-          </div>
-          <div className="flex items-center gap-2">
-            <BsTelephone />
-            +40 744 259 256
-          </div>
-          {/* <div className="flex items-center gap-2">
-          <BsWhatsapp />
-          whatsapp
-        </div> */}
-        </motion.div>
       </div>
 
       <motion.div
-        className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-40 bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] sm:top-[5.3rem] sm:h-[3.25rem] sm:w-[36rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
-        initial={{ y: -100, x: "-50%", opacity: 0 }}
-        animate={{ y: 0, x: "-50%", opacity: 1 }}
-      ></motion.div>
-
-      <nav className="flex fixed top-[0.15rem] left-1/2 h-12 -translate-x-1/2 py-2 sm:top-[5.5rem] sm:h-[initial] sm:py-0">
-        <ul className="flex w-[22rem] flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
-          {links.map((link) => (
-            <motion.li
-              className="h-3/4 flex items-center justify-center relative"
-              key={link.hash}
-              initial={{ y: -100, opacity: 0 }}
-              animate={{ y: 0, opacity: 1 }}
-            >
-              <Link
-                className={clsx(
-                  "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-500 dark:hover:text-gray-300",
-                  {
-                    "text-gray-950 dark:text-gray-200":
-                      activeSection === link.name,
-                  }
-                )}
-                href={link.hash}
-                onClick={() => {
-                  setActiveSection(link.name);
-                  setTimeOfLastClick(Date.now());
-                }}
+        className="flex px-2 py-[2px] bg-white bg-opacity-80 shadow-lg shadow-black/[0.03] backdrop-blur-[0.5rem] rounded-full dark:bg-gray-950 dark:bg-opacity-75"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <nav className="flex">
+          <ul className="flex w-full flex-wrap items-center justify-center gap-y-1 text-[0.9rem] font-medium text-gray-500 sm:w-[initial] sm:flex-nowrap sm:gap-5">
+            {links.map((link) => (
+              <motion.li
+                className="h-3/4 flex items-center justify-center relative"
+                key={link.hash}
+                initial={{ y: -100, opacity: 0 }}
+                animate={{ y: 0, opacity: 1 }}
               >
-                {link.name}
+                <Link
+                  className={clsx(
+                    "flex w-full items-center justify-center px-3 py-3 hover:text-gray-950 transition dark:text-gray-400 dark:hover:text-gray-300",
+                    {
+                      "text-gray-950 dark:text-gray-200":
+                        activeSection === link.name,
+                    }
+                  )}
+                  href={link.hash}
+                  onClick={() => {
+                    setActiveSection(link.name);
+                    setTimeOfLastClick(Date.now());
+                  }}
+                >
+                  {link.name}
 
-                {link.name === activeSection && (
-                  <motion.span
-                    className="bg-gray-100 rounded-full absolute inset-0 -z-10 dark:bg-gray-800"
-                    layoutId="activeSection"
-                    transition={{
-                      type: "spring",
-                      stiffness: 380,
-                      damping: 30,
-                    }}
-                  ></motion.span>
-                )}
-              </Link>
-            </motion.li>
-          ))}
-        </ul>
-      </nav>
+                  {link.name === activeSection && (
+                    <motion.span
+                      className="bg-gray-200 rounded-full absolute inset-0 -z-10 dark:bg-gray-700"
+                      layoutId="activeSection"
+                      transition={{
+                        type: "spring",
+                        stiffness: 380,
+                        damping: 30,
+                      }}
+                    ></motion.span>
+                  )}
+                </Link>
+              </motion.li>
+            ))}
+          </ul>
+        </nav>
+      </motion.div>
+
+      <motion.div
+        className="flex flex-col justify-center items-left gap-2"
+        initial={{ y: -100, opacity: 0 }}
+        animate={{ y: 0, opacity: 1 }}
+      >
+        <div className="flex items-center gap-2  cursor-pointer">
+          <BsTelephone className="h-7 w-7" />
+          <div className="flex flex-col items-center">
+            <p className="font-semibold dark:text-gray-400 dark:hover:text-gray-300">
+              +40 744 259 256
+            </p>
+          </div>
+        </div>
+
+        <div className="flex justify-evenly gap-2">
+          <BsFacebook className="h-6 w-6 cursor-pointer" />
+          <BsWhatsapp className="h-6 w-6 cursor-pointer" />
+        </div>
+      </motion.div>
     </header>
   );
 };
