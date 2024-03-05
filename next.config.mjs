c32fd6f4437566c20588adaeb2474cbe60a8657a
Robert Olteanu <91191507+olteanurobert@users.noreply.github.com>
2024-03-05 17:22:11 +0200

    Update next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    
-        env: {
-            NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: 'AIzaSyDodo5jxKmdXUXC3kXBbq67NgjE7jIs3WQ',
-        },
-      
-};
-
-
+const nextConfig = {};
 
 export default nextConfig;
-
